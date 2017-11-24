16be03de21a23b39291ffa197795cf594d4e09e1
RJ Hallsted <rhallste@student.42.us.org>
2017-11-23 21:24:57 -0800

    Got the basics of the formats working

--- src/ft_snprintf/ap_to_str.c
+++ src/ft_snprintf/ap_to_str.c
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/22 11:18:25 by rhallste          #+#    #+#             */
-/*   Updated: 2017/11/22 14:22:42 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/11/23 21:24:15 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,34 @@ static char *ap_to_str_udec(va_list ap)
 	return (ft_uitoa(va_arg(ap, unsigned int)));
 }
 
+static char *ap_to_str_char(va_list ap)
+{
+	char *str;
+	
+	str = ft_strnew(1);
+	//char is to promotable to int, thus we use int below. Will need
+	//to protect agains things a bit.
+	str[0] = (unsigned char)va_arg(ap, int);
+	return (str);
+}
+
+static char *ap_to_str_octal(va_list ap)
+{
+	//input should not be negative. How to check and inform?
+	return (ft_itoa_base(va_arg(ap, unsigned int), 8));
+}
+
+static char *ap_to_str_pointer(va_list ap)
+{
+	const void *p1;
+	
+	p1 = va_arg(ap, void *);
+	if (p1 != NULL)
+		return (ft_itoa_base((int)p1, 16));
+	else
+		return (ft_strdup("NULL"));
+}
+
 char *ap_to_str(va_list ap, const char *format)
 {
 	//determine expected type.
@@ -37,15 +65,15 @@ char *ap_to_str(va_list ap, const char *format)
 	//return string
 	if (*format == 's' || *format == 'S')
 		return (ap_to_str_str(ap));
-	/* else if (*format == 'p') */
-	/* 	return(ap_to_str_pointer(ap)); */
+	else if (*format == 'p')
+		return(ap_to_str_pointer(ap));
 	else if (*format == 'd' || *format == 'D' || *format == 'i')
 	 	return(ap_to_str_sdec(ap));
-	/* else if (*format == 'o' || *format == 'O') */
-	/* 	return(ap_to_str_octal(ap, 8)); */
+	else if (*format == 'o' || *format == 'O')
+		return(ap_to_str_octal(ap));
 	else if (*format == 'u' || *format == 'U')
 	 	return(ap_to_str_udec(ap));
-	/* else if (*format == 'c' || *format == 'C') */
-	/* 	return(ap_to_str_char(ap)); */
+	else if (*format == 'c' || *format == 'C')
+		return(ap_to_str_char(ap));
 	return NULL;
 }
--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/22 10:53:19 by rhallste          #+#    #+#             */
-/*   Updated: 2017/11/22 14:26:46 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/11/23 21:05:23 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,22 @@ int main(void)
 
 	hold = ft_strnew(100);
 	ft_snprintf(hold, 100, "%u and %u test\n", 3, 13244214);
+	printf("%s", hold);
+	free(hold);
+
+	hold = ft_strnew(100);
+	ft_snprintf(hold, 100, "%c and %C test\n", 'a', '/200');
+	printf("%s", hold);
+	free(hold);
+
+ 	hold = ft_strnew(100);
+	ft_snprintf(hold, 100, "%o And %O and %o test\n", 1, 23, -64);
+	printf("%s", hold);
+	free(hold);
+
+ 	hold = ft_strnew(100);
+	int a = 5;
+	ft_snprintf(hold, 100, "pointer: %p\n", &a);
 	printf("%s", hold);
 	free(hold);
 	return (0);
