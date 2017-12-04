4be19f2ce6c8c6e468ba1e12fffeeeb1a3ddd68d
rjhallsted <rjhallsted@gmail.com>
2017-12-04 12:05:39 -0800

    Added symlink to access current work faster

--- inc/ft_printf.h
+++ inc/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 00:44:01 by rhallste          #+#    #+#             */
-/*   Updated: 2017/12/04 11:32:54 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/12/04 12:05:01 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,19 @@
 # include <wchar.h>
 # include <stdint.h>
 # include <stdarg.h>
+# include <string.h>
 
-int					ft_snprintf(char *str, size_t size,
-								const char *format, ...);
-int					ft_vsnprintf(char *str, size_t size,
-								const char *format, va_list ap);
+struct		ft_format_s
+{
+	char	*flags;
+	size_t	field_width;
+	size_t	precision;
+	int		len_mod;
+	int		conversion;
+};
 
+int			ft_printf(char *format, ...);
+int			ft_snprintf(char *str, size_t size, const char *format, ...);
+int			ft_vsnprintf(char *str, size_t size, const char *format,
+						 va_list ap);
 #endif
