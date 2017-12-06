4f625cf61113eee6ee3251c5108d34c75994f46f
rjhallsted <rjhallsted@gmail.com>
2017-12-05 21:56:02 -0800

    modified testing function for colors and to print failed cases

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 21:13:18 by rhallste          #+#    #+#             */
-/*   Updated: 2017/12/05 21:46:30 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/12/05 21:55:29 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void run_test(const char *format, ...)
 	va_list ap;
 	char	expected[1000];
 	char	actual[1000];
+	int		result;
+	char	*color;
 
 	ft_bzero(expected, 1000);
 	ft_bzero(expected, 1000);
@@ -29,11 +31,16 @@ void run_test(const char *format, ...)
 	va_start(ap, format);
 	ft_vsnprintf(actual, 999, format, ap);
 	va_end(ap);
-	printf("%-30s -> %d\n", format, ft_strcmp(expected, actual) == 0);
+	result = (ft_strcmp(expected, actual) == 0);
+	color = (result) ? "\e[0;32m" : "\e[0;31m";
+	printf("%s%-30s -> %d%s\n", color, format, result, "\e[0;m");
+	if (!result)
+		printf("%s\t%s%s\n", color, actual, "\e[0;m");
 }
 
 int main(void)
 {
+	run_test("No spec:");
 	run_test("Ints: %d, %i", 5, -12);
 	run_test("Char: %c", 'a');
 	run_test("Long: %ld, %D", (long)12351253312, (long)-123423523523);
