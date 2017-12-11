1a9d02e787880ca8a1d5a7d05877503b1f713413
rjhallsted <rjhallsted@gmail.com>
2017-12-11 15:07:42 -0800

    freed str inside ft_printf

--- src/ft_printf/ft_printf.c
+++ src/ft_printf/ft_printf.c
@@ -6,11 +6,12 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/20 19:01:29 by rhallste          #+#    #+#             */
-/*   Updated: 2017/12/11 15:05:03 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/12/11 15:07:14 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h>
 #include "../../inc/libft.h"
 
 int	ft_printf(const char *format, ...)
@@ -24,5 +25,6 @@ int	ft_printf(const char *format, ...)
 	len = ft_vsnprintf(str, 1000, format, ap);
 	va_end(ap);
 	write(1, str, len);
+	free(str);
 	return (len);
 }
