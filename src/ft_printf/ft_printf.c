495f8faedada393fe6a2d7829a1d7a33975d7265
rjhallsted <rjhallsted@gmail.com>
2017-12-13 20:31:02 -0800

    Removed printf calls in ft_printf

--- src/ft_printf/ft_printf.c
+++ src/ft_printf/ft_printf.c
@@ -6,15 +6,13 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/20 19:01:29 by rhallste          #+#    #+#             */
-/*   Updated: 2017/12/13 20:14:26 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/12/13 20:30:41 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
 #include "../../inc/libft.h"
-
-#include <stdio.h>
 
 int	ft_printf(const char *format, ...)
 {
@@ -26,7 +24,6 @@ int	ft_printf(const char *format, ...)
 	va_start(ap, format);
 	len = ft_vsnprintf(str, 1000, format, ap);
 	va_end(ap);
-	printf("len: %zu\n", len);
 	write(1, str, len);
 	free(str);
 	return (len);
