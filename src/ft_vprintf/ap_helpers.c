38a9f217d33498e0c0d1399baa60e769efc76a46
rjhallsted <rjhallsted@gmail.com>
2017-12-15 23:10:13 -0800

    Should write null chars now

--- src/ft_vprintf/ap_helpers.c
+++ src/ft_vprintf/ap_helpers.c
@@ -6,10 +6,11 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/14 23:50:10 by rhallste          #+#    #+#             */
-/*   Updated: 2017/12/15 22:44:00 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/12/15 23:09:33 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include <stdlib.h>
 #include "../../inc/libft.h"
 
@@ -17,7 +18,9 @@ void	ft_vprintf_apchar_to_str(t_format fmt, intmax_t signed_int, char **s)
 {
 	*s = ft_strnew(1);
 	**s = (unsigned char)signed_int;
-	fmt.is_nullchar = (**s == '\0');
+//	fmt.is_nullchar = (**s == '\0');
+	if (**s == '\0')
+		write(1, *s, 1);
 	ft_vprintf_process_return(fmt, s);
 }
 
