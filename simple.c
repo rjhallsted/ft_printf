f20172a0fa6707cd578e8e436bf427d601d067ce
rjhallsted <rjhallsted@gmail.com>
2017-12-15 23:33:52 -0800

    Extra test

--- simple.c
+++ simple.c
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/15 23:01:02 by rhallste          #+#    #+#             */
-/*   Updated: 2017/12/15 23:28:53 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/12/15 23:33:20 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ int main(void)
 	ft_printf("%s\n", "TESTING it out");
 	ft_printf("%+3d\n", 5);
 	ft_printf("%+i\n", 42);
-	ft_printf("%*d\n", 3);
+	ft_printf("%+*1d\n", 3);
 	return (0);
 }
