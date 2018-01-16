204492140c8ea5160bcb916dc21fdc97934efbfa
rjhallsted <rjhallsted@gmail.com>
2018-01-15 16:50:50 -0800

    zero flag working correctly

--- src/ft_vprintf/process_return.c
+++ src/ft_vprintf/process_return.c
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/14 23:36:44 by rhallste          #+#    #+#             */
-/*   Updated: 2018/01/15 16:18:56 by rhallste         ###   ########.fr       */
+/*   Updated: 2018/01/15 16:49:59 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,20 +29,28 @@ static void	handle_sharp_flag(t_format *format, char **s)
 
 static void	handle_flags(t_format *format, char **s)
 {
+	char *tmp;
 	handle_sharp_flag(format, s);
 	if (format->flags[FLAGS_SPACE_KEY] && **s != '-' && format->field_width > -1
 		&& !format->is_nullchar && format->conversion != UINT_T)
-		*s = ft_strjoinfree(" ", *s, 2);
-	if (ft_strchr(format->flags, '0') && !ft_strchr(format->flags, '-')
-		&& format->field_width > -1 && format->precision == -1)
 	{
-		ft_strreplace(*s, ' ', '0');
-		if (ft_strchr(*s, '-'))
-		{
-			ft_strreplace(*s, '-', '0');
-			**s = '-';
-		}
+		tmp = (format->flags[FLAGS_ZERO_KEY]) ? "0" : " ";
+		*s = ft_strjoinfree(tmp, *s, 2);
 	}
+	if (format->flags[FLAGS_ZERO_KEY] && format->field_width > (int)ft_strlen(*s)
+		&& format->precision == -1 && !(format->flags[FLAGS_MINUS_KEY]))
+	{
+		tmp = ft_xstring('0', format->field_width - ft_strlen(*s));
+		*s = ft_strjoinfree(tmp, *s, 3);
+		tmp = ft_strdup("0x");
+		tmp[1] = (format->disp_mod == HEX_UP_DISP) ? 'X' : tmp[1];
+		if ((format->disp_mod == HEX_DISP || format->disp_mod == HEX_UP_DISP)
+			&& ft_strstr(*s, tmp))
+		{
+			ft_memset(ft_strstr(*s, tmp), '0', 2);
+			ft_strncpy(*s, tmp, 2);
+		}
+	}	
 	if (ft_strchr(format->flags, '+') && !ft_strchr(*s, '-') && format->conversion != UINT_T
 		&& ((**s == '0' && ft_strlen(*s) > 1) || **s == ' '))
 	{
