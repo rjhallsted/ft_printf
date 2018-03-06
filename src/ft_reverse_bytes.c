4b93ed13fb909023c3a4e73102407792f04604df
rjhallsted <rjhallsted@gmail.com>
2018-03-05 22:04:38 -0800

    bug fix?

--- src/ft_reverse_bytes.c
+++ src/ft_reverse_bytes.c
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 21:41:18 by rhallste          #+#    #+#             */
-/*   Updated: 2018/03/05 21:56:30 by rhallste         ###   ########.fr       */
+/*   Updated: 2018/03/05 22:04:22 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,16 @@
 void	ft_reverse_bytes(void *mem, size_t size)
 {
 	size_t			i;
+	unsigned char	tmp;
 	unsigned char	*memstr;
 
 	memstr = mem;
 	i = 0;
 	while (i < size / 2)
 	{
-		memstr[i] ^= memstr[(size - 1) - i];
-		memstr[(size - 1) - i] ^= memstr[i];
-		memstr[i] ^= memstr[(size - 1) - i];
+		tmp = memstr[i];
+		memstr[i] = memstr[(size - 1) - i];
+		memstr[(size - 1) - i] = tmp;
 		i++;
 	}
 }
