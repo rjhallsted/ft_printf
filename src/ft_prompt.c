8f6ee97a8d0094e1e16aea3c1cb0fdf047db7f8f
RJ Hallsted <rhallste@student.42.us.org>
2018-03-07 02:06:54 -0800

    norm

--- src/ft_prompt.c
+++ src/ft_prompt.c
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 22:56:58 by rhallste          #+#    #+#             */
-/*   Updated: 2018/03/06 23:00:12 by rhallste         ###   ########.fr       */
+/*   Updated: 2018/03/07 02:06:40 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 char	*ft_prompt(const char *prompt)
 {
 	char *response;
-	
+
 	ft_printf("%s", prompt);
 	get_next_line(STDIN_FILENO, &response);
 	return (response);
